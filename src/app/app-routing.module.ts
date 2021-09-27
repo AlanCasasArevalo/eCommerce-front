3ec957d91c467090c1218d51fb12438ebcba0fc7
Alan Casas <alancasasarevalo@gmail.com>
2021-09-27 17:57:02 +0200

    * Routes were set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./@public/pages/home/home.module').then(module => module.HomeModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./@public/pages/contact/contact.module').then(module => module.ContactModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
