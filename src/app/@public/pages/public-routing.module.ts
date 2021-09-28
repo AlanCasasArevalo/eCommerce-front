e58f4df215cad0caae8c661433edce5ba3b9b60b
Alan Casas <alancasasarevalo@gmail.com>
2021-09-28 10:55:02 +0200

    * Basic structure project was done

--- src/app/@public/pages/public-routing.module.ts
+++ src/app/@public/pages/public-routing.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AdminComponent} from "../../@admin/pages/admin.component";
+import {PublicComponent} from "./public.component";
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(module => module.HomeModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./contact/contact.module').then(module => module.ContactModule)
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(module => module.HomeModule)
+      },
+      {
+        path: 'contact',
+        loadChildren: () => import('./contact/contact.module').then(module => module.ContactModule)
+      }
+    ]
   }
 ];
 
